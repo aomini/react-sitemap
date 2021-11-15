3499f8b581cc21292e228cba3b5b11bdd48f408e
Sandesh Pokharel <s.pokharel1@codavatar.tech>
2021-11-15 13:13:23 +0545

    Option to add a xslt stylesheet file on sitemap

--- src/sitemap/withSitemapIndex.js
+++ src/sitemap/withSitemapIndex.js
@@ -1,4 +1,9 @@
 const withSitemapIndex = (sitemaps) => {
-  return `<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemaps}</sitemapindex>`;
+  const { stylesheet } = loadFile(getConfigPath());
+  let xsltElement = "";
+  if (stylesheet) {
+    xsltElement = `<?xml-stylesheet type="text/xsl" href="${stylesheet}" ?>`;
+  }
+  return `<?xml version="1.0" encoding="UTF-8"?>${xsltElement}<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemaps}</sitemapindex>`;
 };
 export default withSitemapIndex;
