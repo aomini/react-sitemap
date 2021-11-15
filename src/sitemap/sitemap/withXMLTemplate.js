b02f8587352584fce52aa2879133f0902e073546
Sandesh Pokharel <s.pokharel1@codavatar.tech>
2021-11-15 15:51:17 +0545

    Checking availibility of xsl file on stylesheet options

--- src/sitemap/sitemap/withXMLTemplate.js
+++ src/sitemap/sitemap/withXMLTemplate.js
@@ -3,9 +3,10 @@ import getConfigPath from "../paths/getConfigPath";
 
 const withXMLTemplate = (content) => {
   const { urlsetStylesheet } = loadFile(getConfigPath());
-  const xsltElement = urlsetStylesheet
-    ? `\n<?xml-stylesheet type="text/xsl" href="${urlsetStylesheet}" ?>\n`
-    : "\n";
+  const xsltElement =
+    urlsetStylesheet && urlsetStylesheet.includes(".xsl")
+      ? `\n<?xml-stylesheet type="text/xsl" href="${urlsetStylesheet}" ?>\n`
+      : "\n";
   return `<?xml version="1.0" encoding="UTF-8"?>${xsltElement}<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${content}</urlset>`;
 };
 
--- src/sitemap/withSitemapIndex.js
+++ src/sitemap/withSitemapIndex.js
@@ -3,9 +3,10 @@ import getConfigPath from "./paths/getConfigPath";
 
 const withSitemapIndex = (sitemaps) => {
   const { sitemapStylesheet } = loadFile(getConfigPath());
-  const xsltElement = sitemapStylesheet
-    ? `\n<?xml-stylesheet type="text/xsl" href="${sitemapStylesheet}" ?>\n`
-    : "\n";
+  const xsltElement =
+    sitemapStylesheet && sitemapStylesheet.includes(".xsl")
+      ? `\n<?xml-stylesheet type="text/xsl" href="${sitemapStylesheet}" ?>\n`
+      : "\n";
   return `<?xml version="1.0" encoding="UTF-8"?>${xsltElement}<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemaps}</sitemapindex>`;
 };
 export default withSitemapIndex;
